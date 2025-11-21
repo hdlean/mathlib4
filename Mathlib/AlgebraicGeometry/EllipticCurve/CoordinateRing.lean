711d9ecc23f43fd94d852e1f49864db60afdc2cd
David Kurniadi Angdinata <dka31@cantab.ac.uk>
2025-11-21 17:18:03 +0100

    Prove that the function field is a fraction ring of the coordinate ring

--- Mathlib/AlgebraicGeometry/EllipticCurve/CoordinateRing.lean
+++ Mathlib/AlgebraicGeometry/EllipticCurve/CoordinateRing.lean
@@ -3,11 +3,11 @@ Copyright (c) 2025 David Kurniadi Angdinata, Sriram Chinthalagiri Venkata, and J
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: David Kurniadi Angdinata, Sriram Chinthalagiri Venkata, Junyan Xu
 -/
+import Mathlib.Algebra.Module.LocalizedModule.Exact
 import Mathlib.AlgebraicGeometry.EllipticCurve.Affine.Point
 import Mathlib.AlgebraicGeometry.EllipticCurve.NormalForms
 import Mathlib.RingTheory.DedekindDomain.IntegralClosure
 import Mathlib.RingTheory.Localization.Algebra
-import Mathlib.Algebra.Algebra.Field
 /-!
 # Coordinate ring of an elliptic curve
 
@@ -17,8 +17,69 @@ See https://leanprover.zulipchat.com/#narrow/channel/217875-Is-there-code-for-X.
 for a proof outline.
 -/
 
-open Polynomial
-open scoped Polynomial.Bivariate NoZeroDivisors
+open Ideal Polynomial
+
+open scoped Bivariate nonZeroDivisors
+
+attribute [local instance] algebra
+
+noncomputable section CommRing
+
+variable {R A : Type*} [CommRing R] [CommRing A] [Algebra R A]
+
+abbrev Ideal.Quotient.algebraQuotientOfMapLE {I : Ideal R} {J : Ideal A}
+    (h : I.map (algebraMap R A) ≤ J) : Algebra (R ⧸ I) (A ⧸ J) :=
+  Ideal.Quotient.algebraQuotientOfLEComap <| Ideal.le_comap_of_map_le h
+
+abbrev Ideal.Quotient.isScalarTowerQuotient {I : Ideal R} {J : Ideal A}
+    (h : I.map (algebraMap R A) ≤ J) :
+    have : Algebra (R ⧸ I) (A ⧸ J) := algebraQuotientOfMapLE h
+    IsScalarTower R (R ⧸ I) (A ⧸ J) :=
+  let := algebraQuotientOfMapLE h
+  IsScalarTower.of_algebraMap_eq <| congrFun rfl
+
+abbrev Ideal.Quotient.isScalarTowerPolynomialQuotient {I : Ideal R[X]} {J : Ideal A[X]}
+    (h : I.map (algebraMap R[X] A[X]) ≤ J) :
+    have : Algebra (R[X] ⧸ I) (A[X] ⧸ J) := algebraQuotientOfMapLE h
+    IsScalarTower R (R[X] ⧸ I) (A[X] ⧸ J) :=
+  let := algebraQuotientOfMapLE h
+  .of_algebraMap_eq' <| congrArg (algebraMap ..).comp <| IsScalarTower.algebraMap_eq R R[X] _
+
+instance (I : Ideal R[X]) : Algebra (R[X] ⧸ I) (A[X] ⧸ I.map (mapRingHom <| algebraMap R A)) :=
+  Ideal.Quotient.algebraQuotientOfMapLE le_rfl
+
+instance (I : Ideal R[X]) :
+    IsScalarTower R (R[X] ⧸ I) (A[X] ⧸ I.map (mapRingHom <| algebraMap R A)) :=
+  Ideal.Quotient.isScalarTowerPolynomialQuotient le_rfl
+
+instance (p : R[X]) : Algebra (AdjoinRoot p) (AdjoinRoot <| p.map <| algebraMap R A) :=
+  Ideal.Quotient.algebraQuotientOfMapLE <| by simp [Ideal.map_span]
+
+instance (p : R[X]) : IsScalarTower R (AdjoinRoot p) (AdjoinRoot <| p.map <| algebraMap R A) :=
+  Ideal.Quotient.isScalarTowerPolynomialQuotient <| by simp [Ideal.map_span]
+
+def AdjoinRoot.isFractionRing [IsDomain R] [IsFractionRing R A] {p : R[X]} (prime : Prime p)
+    (degree : degree p ≠ 0) :
+    IsFractionRing (AdjoinRoot p) (AdjoinRoot <| p.map <| algebraMap R A) := by
+  have ne_zero : p ≠ 0 := fun h ↦ not_prime_zero <| h ▸ prime
+  have isDomain : IsDomain <| AdjoinRoot p := isDomain_of_prime prime
+  have isAlgebraic (x : AdjoinRoot p) : IsAlgebraic R x :=
+    Algebra.isAlgebraic_adjoin_singleton_iff.mpr (isAlgebraic_root ne_zero) x <|
+      by simpa only [adjoinRoot_eq_top] using Algebra.mem_top
+  have isLocalization := isLocalization R⁰ A
+  rw [IsFractionRing, ← IsLocalization.iff_of_le_of_exists_dvd]
+  · exact IsLocalization.of_surjective (R⁰.map C) A[X] _ Quotient.mk_surjective _
+      Quotient.mk_surjective rfl <| fun _ h ↦ by
+        rcases mem_span_singleton'.mp <| Quotient.eq_zero_iff_mem.mp h with ⟨_, rfl⟩
+        exact mul_mem_left _ _ <| mem_map_of_mem _ <| Quotient.mk_singleton_self ..
+  · rintro _ ⟨_, ⟨_, h, rfl⟩, rfl⟩
+    exact mem_nonZeroDivisors_of_ne_zero <| mem_nonZeroDivisors_iff_ne_zero.mp h
+      ∘ (injective_iff_map_eq_zero _).mp (of.injective_of_degree_ne_zero degree) _
+  · intro _ h
+    rcases (isAlgebraic _).exists_nonzero_dvd h with ⟨_, h, _, h'⟩
+    exact ⟨_, ⟨_, ⟨_, mem_nonZeroDivisors_of_ne_zero h, rfl⟩, rfl⟩, _, h'⟩
+
+end CommRing
 
 noncomputable section
 
@@ -38,98 +99,78 @@ namespace Affine
 /- A type synonym of WeierstrassCurve to give access to affine versions of the Weierstrass
 polynomial and coordinate ring, etc. -/
 
-variable {K : Type*} [Field K] (E : WeierstrassCurve.Affine K)
+variable {R : Type*} [CommRing R] [IsDomain R] [UniqueFactorizationMonoid R] (E : Affine R)
 
-notation:10000 K"(X)" => FractionRing K[X]
+notation:10000 R"(X)" => FractionRing R[X]
 
-/-- Another implementation of the function field of a Weierstrass curve, as `K(X)[Y]` modulo
+/-- Another implementation of the function field of a Weierstrass curve, as `R(X)[Y]` modulo
 the Weierstrass polynomial. -/
-abbrev FunctionField' := AdjoinRoot <| E.polynomial.map (algebraMap K[X] K(X))
--- another implementation could be K(X) ⊗[K[X]] E.CoordinateRing
+abbrev FunctionField' := AdjoinRoot <| E.polynomial.map <| algebraMap R[X] R(X)
+-- another implementation could be R(X) ⊗[R[X]] E.CoordinateRing
 
-instance : Fact (Irreducible <| E.polynomial.map (algebraMap K[X] K(X))) :=
-  ⟨monic_polynomial.irreducible_iff_irreducible_map_fraction_map.mp irreducible_polynomial⟩
+def irreducible_polynomial' : Irreducible <| E.polynomial.map <| algebraMap R[X] R(X) :=
+  monic_polynomial.irreducible_iff_irreducible_map_fraction_map.mp irreducible_polynomial
 
-  -- use Gauss lemma: Polynomial.Monic.irreducible_iff_irreducible_map_fraction_map
+def prime_polynomial' : Prime <| E.polynomial.map <| algebraMap R[X] R(X) :=
+  E.irreducible_polynomial'.prime
 
-attribute [local instance] Polynomial.algebra in
-instance : Algebra E.CoordinateRing E.FunctionField' := by
-  apply Ideal.Quotient.algebraQuotientOfLEComap
-  apply Ideal.le_comap_of_map_le
-  rw [Ideal.map_span, algebraMap_def, coe_mapRingHom, Set.image_singleton]
+instance : Fact <| Irreducible <| E.polynomial.map <| algebraMap R[X] R(X) :=
+  ⟨E.irreducible_polynomial'⟩
 
-attribute [local instance] Polynomial.algebra in
-instance {R K} [CommRing R] [CommRing K] [Algebra R K] [IsFractionRing R K] {p : R[X]} :
-Algebra (AdjoinRoot p)  (AdjoinRoot <| p.map <| algebraMap R K):= by
+instance : Algebra E.CoordinateRing E.FunctionField' :=
+  inferInstance
 
-  apply Ideal.Quotient.algebraQuotientOfLEComap
-  apply Ideal.le_comap_of_map_le
-  rw [Ideal.map_span, algebraMap_def, coe_mapRingHom, Set.image_singleton]
-attribute [local instance] Polynomial.algebra in
+instance : IsScalarTower R[X] E.CoordinateRing E.FunctionField' :=
+  inferInstance
 
--- instance {R K} [CommRing R] [CommRing K] [Algebra R K] [IsFractionRing R K] {p : R[X]} (h : Irreducible p) :
---     IsFractionRing (AdjoinRoot p) (AdjoinRoot <| p.map <| algebraMap R K) := by
+instance : Module.Free R[X] E.CoordinateRing :=
+  .of_basis <| CoordinateRing.basis E
 
+instance : Module.Finite R[X] E.CoordinateRing :=
+  monic_polynomial.finite_adjoinRoot
 
+instance : FiniteDimensional R(X) E.FunctionField' :=
+  (monic_polynomial.map _).finite_adjoinRoot
 
-example {R} [CommRing R] {p : R[X]} {h : Irreducible p} :
-    (nonZeroDivisors R).map ((Ideal.Quotient.mk _).comp C) = nonZeroDivisors (R[X] ⧸ Ideal.span {p})
-    := by
+instance : IsFractionRing E.CoordinateRing E.FunctionField' :=
+  AdjoinRoot.isFractionRing irreducible_polynomial.prime <| E.degree_polynomial ▸ two_ne_zero
 
-  sorry
+instance : FaithfulSMul E.CoordinateRing E.FunctionField' :=
+  inferInstance
 
-instance : IsScalarTower K[X] E.CoordinateRing E.FunctionField' := by
-  sorry
+-- may be unnecessary for the final goal
+theorem isIntegral_coordinateRing_iff {f : E.FunctionField'} :
+    IsIntegral E.CoordinateRing f ↔ IsIntegral R[X] f := by
+  sorry -- use that E.CoordinateRing is integral over R[X]
 
-instance : Module.Free K[X] E.CoordinateRing := .of_basis (CoordinateRing.basis E)
-instance : Module.Finite K[X] E.CoordinateRing := E.monic_polynomial.finite_adjoinRoot
-instance : FiniteDimensional K(X) E.FunctionField' := (E.monic_polynomial.map _).finite_adjoinRoot
+-- this deals with the q = 0 case
+theorem isIntegral_algebraMap_iff {p : R(X)} :
+    IsIntegral R[X] (algebraMap _ E.FunctionField' p) ↔ p ∈ toRatFunc.range := by
+  sorry -- use that R[X] is integrally closed in R(X), and `isIntegral_algHom_iff`
 
-variable (p q : K(X))
+variable (p q : R(X))
 
 -- rename to `addMulY`?
 def comb : E.FunctionField' := p • 1 + q • .mk _ X
 
+def trace : R(X) :=
+  2 * p - q * (C E.a₁ * X + C E.a₃).toRatFunc
+
+def norm : R(X) :=
+  p ^ 2 - p * q * (C E.a₁ * X + C E.a₃).toRatFunc -
+    q ^ 2 * (X ^ 3 + C E.a₂ * X ^ 2 + C E.a₄ * X + C E.a₆).toRatFunc
+
 -- An arbitrary element of the function field can be written in the form p(X) + q(X)Y
-theorem FunctionField'.exists_comb_eq (f : E.FunctionField') :
-    ∃ p q : K(X), E.comb p q = f := by
+theorem FunctionField'.exists_comb_eq (f : E.FunctionField') : ∃ p q : R(X), E.comb p q = f := by
   sorry
   -- mimic the proof of `CoordinateRing.exists_smul_basis_eq`
   -- may need to develop some basis API
 
-instance : FaithfulSMul E.CoordinateRing E.FunctionField' :=
-  (faithfulSMul_iff_algebraMap_injective ..).mpr <| by
-    sorry
-  /- Write an element in the coordinate ring K[E] as p+qY using
-    `CoordinateRing.exists_smul_basis_eq`, and show its image can't be zero unless p = q = 0. -/
-
-instance : IsFractionRing E.CoordinateRing E.FunctionField' := .of_field _ _ <| by
-  sorry
-
--- may be unnecessary for the final goal
-theorem isIntegral_coordinateRing_iff {f : E.FunctionField'} :
-    IsIntegral E.CoordinateRing f ↔ IsIntegral K[X] f := by
-  sorry -- use that E.CoordinateRing is integral over K[X]
-
--- this deals with the q = 0 case
-theorem isIntegral_algebraMap_iff {p : K(X)} :
-    IsIntegral K[X] (algebraMap _ E.FunctionField' p) ↔ p ∈ toRatFunc.range := by
-  sorry -- use that K[X] is integrally closed in K(X), and `isIntegral_algHom_iff`
-
-def trace : K(X) :=
-  2 * p - q * (C E.a₁ * X + C E.a₃).toRatFunc
-
-def norm : K(X) :=
-  p ^ 2 - p * q * (C E.a₁ * X + C E.a₃).toRatFunc -
-    q ^ 2 * (X ^ 3 + C E.a₂ * X ^ 2 + C E.a₄ * X + C E.a₆).toRatFunc
-
 -- If q ≠ 0, the minimal polynomial of f = p + qY is quadratic, given by Z² - Tr(f)Z + N(f).
 theorem minpoly_comb (hq : q ≠ 0) :
-    minpoly K(X) (E.comb p q) = X ^ 2 - C (E.trace p q) * X + C (E.norm p q) := by
-  refine (minpoly.eq_of_irreducible_of_monic ?_ ?_ ?_).symm
-  · sorry
-  · sorry
-  · sorry
+    minpoly R(X) (E.comb p q) = X ^ 2 - C (E.trace p q) * X + C (E.norm p q) := by
+  sorry
+  -- refine (minpoly.eq_of_irreducible_of_monic ?_ ?_ ?_).symm
 
 theorem trace_sq_sub_four_mul_norm :
     E.trace p q ^ 2 - 4 * E.norm p q = q ^ 2 * E.twoTorsionPolynomial.toPoly.toRatFunc := by
@@ -140,73 +181,55 @@ theorem isIntegral_of_sq_add_mem_range {R A} [CommRing R] [Ring A] [Algebra R A]
     IsIntegral R a := by
   sorry
 
-/- If q and N(p+qY) lie in K[X], then p satisfies a monic quadratic equation
-with coefficients in K[X], so p is integral over K[X] and therefore in K[X]. -/
+/- If q and N(p+qY) lie in R[X], then p satisfies a monic quadratic equation
+with coefficients in R[X], so p is integral over R[X] and therefore in R[X]. -/
 theorem left_mem_of_right_mem_of_norm_mem (hq : q ∈ toRatFunc.range)
     (hn : E.norm p q ∈ toRatFunc.range) : p ∈ toRatFunc.range := by
   sorry -- obtain from hq, rewrite E.norm, then apply `isIntegral_of_sq_add_mem_range`
 
-variable {p q} (int : IsIntegral K[X] (E.comb p q))
-include int
-
-theorem trace_mem_of_isIntegral : E.trace p q ∈ toRatFunc.range := by
+theorem trace_mem_of_isIntegral {p q : R(X)} (int : IsIntegral R[X] <| E.comb p q) :
+    E.trace p q ∈ toRatFunc.range := by
   sorry -- use minpoly.isIntegrallyClosed_eq_field_fractions'
 
-theorem norm_mem_of_isIntegral : E.norm p q ∈ toRatFunc.range := by
+theorem norm_mem_of_isIntegral {p q : R(X)} (int : IsIntegral R[X] <| E.comb p q) :
+    E.norm p q ∈ toRatFunc.range := by
   sorry -- ditto
 
-variable [E.IsElliptic]
+variable {K : Type*} [Field K] (E : Affine K) [E.IsElliptic] {p q : K(X)}
+  (int : IsIntegral K[X] <| E.comb p q)
+
+include int
 
 section IsUnit2
 
 variable (h2 : IsUnit (2 : K))
 
-include h2 in
-omit int in
--- `E.twoTorsionPolynomial` is nonzero (if char K ≠ 2) and separable since E is an elliptic curve.
+include h2
+
 theorem separable_twoTorsionPolynomial : E.twoTorsionPolynomial.toPoly.Separable := by
   have : NeZero (2 : K) := by
     exact ⟨IsUnit.ne_zero h2⟩
   have : NeZero (4 : K) := by
     rw [show (4 : K) = 2 * 2 by norm_num1]
     exact NeZero.mul
-
-  have h: E.twoTorsionPolynomial.discr ≠ 0 := by apply WeierstrassCurve.twoTorsionPolynomial_discr_ne_zero _ _
-                                                 (expose_names; exact (isElliptic_iff E).mp inst_1)
-                                                 exact h2
-
-  --have :(twoTorsionPolynomial E).toPoly ≠ 0:= by contrapose! h; apply (Cubic.toPoly_eq_zero_iff _).mp at h;rw [h]; rw [Cubic.discr]; ext
-
-  apply (Polynomial.nodup_aroots_iff_of_splits (K:=SplittingField E.twoTorsionPolynomial.toPoly) _ _).mp
-  have := (Cubic.splits_iff_roots_eq_three (F:=K) (K:=SplittingField E.twoTorsionPolynomial.toPoly) (φ:=algebraMap _ _) (P := E.twoTorsionPolynomial) four_ne_zero).mp (Polynomial.IsSplittingField.splits _ _)
+  have h: E.twoTorsionPolynomial.discr ≠ 0 := by
+    apply WeierstrassCurve.twoTorsionPolynomial_discr_ne_zero _ _
+    (expose_names; apply (isElliptic_iff E).mp inst_1)
+    exact h2
+  apply (Polynomial.nodup_aroots_iff_of_splits (K := SplittingField E.twoTorsionPolynomial.toPoly) _ _).mp
+  have := (Cubic.splits_iff_roots_eq_three (F := K) (K := SplittingField E.twoTorsionPolynomial.toPoly) (φ:=algebraMap _ _) (P := E.twoTorsionPolynomial) four_ne_zero).mp (Polynomial.IsSplittingField.splits _ _)
   rcases this with ⟨_ , _ , _ , w⟩
   convert (Cubic.discr_ne_zero_iff_roots_nodup (P := E.twoTorsionPolynomial) (φ:=algebraMap _ _) (F:=K) (K:=SplittingField E.twoTorsionPolynomial.toPoly) four_ne_zero w).mp _
-  rw [twoTorsionPolynomial]; rw [Cubic.map_roots];assumption;swap; exact Polynomial.IsSplittingField.splits _ _
-  apply Cubic.ne_zero_of_a_ne_zero; exact four_ne_zero
-  --contrapose! h; apply (Cubic.toPoly_eq_zero_iff _).mp at h;rw [h]; rw [← Cubic.of_d_eq_zero' (R:=K)]; rw [Cubic.discr];
+  rw [twoTorsionPolynomial]
+  rw [Cubic.map_roots]
+  assumption
+  swap
+  exact Polynomial.IsSplittingField.splits _ _
+  apply Cubic.ne_zero_of_a_ne_zero
+  exact four_ne_zero
 
-
-
-
-  /- use `WeierstrassCurve.twoTorsionPolynomial_discr_ne_zero`,
-    `Cubic.discr_ne_zero_iff_roots_nodup`,
-    `Polynomial.nodup_aroots_iff_of_splits` with `AlgebraicClosure K` -/
-
-/- By `trace_sq_sub_four_mul_norm`, `q² * E.twoTorsionPolynomial` is in K[X],
-but `E.twoTorsionPolynomial` is separable, hence squarefree, so q ∈ K[X].
-Use `Polynomial.Separable.squarefree` and
-`UniqueFactorizationMonoid.squarefree_iff_nodup_normalizedFactors`.
-Maybe extract a lemma for `UniqueFactorizationMonoid`. -/
-include h2 in
+/- Maybe extract a lemma for `UniqueFactorizationMonoid`. -/
 theorem right_mem_of_isIntegral : q ∈ toRatFunc.range := by
-  -- have : ∃ f g : K[X], q=toRatFunc f / toRatFunc g ∧ IsCoprime f g := by
-  --     rcases IsFractionRing.exists_reduced_fraction (A:= K[X]) (x := q) with ⟨a,b,A,B⟩; use a;use b
-  --     constructor
-  --     swap; exact A.isCoprime; symm; convert B;rw [← IsLocalization.mk'_spec' K(X) a b];rw [mul_div_cancel_left₀];
-  --     subst B
-  --     simp_all only [isUnit_iff_ne_zero, ne_eq, IsFractionRing.mk'_eq_div, FaithfulSMul.algebraMap_eq_zero_iff,
-  --       nonZeroDivisors.coe_ne_zero, not_false_eq_true]
-  -- rcases this with ⟨f, g, h⟩
   rcases IsFractionRing.exists_reduced_fraction (A:= K[X]) (x := q) with ⟨f,g,h,h'⟩
   have jr: ∃ h : K[X], f * f * E.twoTorsionPolynomial.toPoly = h * (g * g) := by
     have := trace_sq_sub_four_mul_norm E p q
@@ -222,29 +245,16 @@ theorem right_mem_of_isIntegral : q ∈ toRatFunc.range := by
     rw [← IsLocalization.mk'_spec' K(X) f g]
     --simp
     ring1
-
-
-
   have hu : (g : K[X]) * g ∣ E.twoTorsionPolynomial.toPoly:= by
     rcases jr with ⟨l, hi⟩
     apply IsCoprime.dvd_of_dvd_mul_left (y := f * f) <| by
       apply IsCoprime.mul_right <;> exact h.isCoprime.symm.mul_left h.isCoprime.symm
     use l; linear_combination hi
-
-
   have he : IsUnit (g : K[X]) := by
-    apply Polynomial.Separable.squarefree (E.separable_twoTorsionPolynomial h2) _ hu
+    apply Polynomial.Separable.squarefree (E.separable_twoTorsionPolynomial int h2) _ hu
   rw [← h']; use f * he.unit⁻¹
   aesop
 
-
-
-#check map_div'
-
-/- Since q ∈ K[X], `norm_meh2.unitm_of_isIntegral` shows that p satisfies a monic quadratic equation
-with coefficients in K[X], so p is integral over K[X] and therefore in K[X]. -/
-
-include h2 in
 theorem left_mem_of_isIntegral : p ∈ toRatFunc.range := by
   have ho: q ∈ toRatFunc.range:= by apply right_mem_of_isIntegral E int h2
   rcases ho with ⟨f , hf⟩
@@ -259,6 +269,7 @@ theorem left_mem_of_isIntegral : p ∈ toRatFunc.range := by
   rw [inv_mul_cancel₀]
   simp
   exact h2.ne_zero
+
 end IsUnit2
 
 section Char2
@@ -346,8 +357,11 @@ instance : IsIntegrallyClosed E.CoordinateRing := by
   · exact (isIntegrallyClosed_iff_isIntegrallyClosedIn _).mpr
       (CoordinateRing.isIntegrallyClosedIn E <| .inl <| Ne.isUnit h2)
 
-example : IsIntegralClosure E.CoordinateRing K[X] E.FunctionField' := inferInstance
-example : IsIntegrallyClosedIn E.CoordinateRing E.FunctionField' := inferInstance
+instance : IsIntegralClosure E.CoordinateRing K[X] E.FunctionField' :=
+  inferInstance
+
+instance : IsIntegrallyClosedIn E.CoordinateRing E.FunctionField' :=
+  inferInstance
 
 instance : Algebra.IsSeparable K(X) E.FunctionField' := by
   sorry
@@ -356,6 +370,6 @@ instance : Algebra.IsSeparable K(X) E.FunctionField' := by
     so the linear term of the minimal polynomial of Y does not vanish. -/
 
 instance : IsDedekindDomain E.CoordinateRing :=
-  IsIntegralClosure.isDedekindDomain K[X] K(X) E.FunctionField' _
+  IsIntegralClosure.isDedekindDomain K[X] K(X) E.FunctionField' E.CoordinateRing
 
 end WeierstrassCurve.Affine
