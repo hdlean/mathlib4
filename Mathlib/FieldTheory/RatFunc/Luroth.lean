8fb6a828e32490ad7b0d724560aae10fa823fd9b
Justus Springer <justusspringer@gmx.de>
2026-03-02 12:25:11 +0000

    fix

--- Mathlib/FieldTheory/RatFunc/Luroth.lean
+++ Mathlib/FieldTheory/RatFunc/Luroth.lean
@@ -9,6 +9,7 @@ public import Mathlib.LinearAlgebra.Basis.Basic
 public import Mathlib.FieldTheory.RatFunc.AsPolynomial
 public import Mathlib.FieldTheory.Relrank
 public import Mathlib.Algebra.Polynomial.Bivariate
+public import Mathlib.Algebra.Polynomial.BigOperators
 public import Mathlib.Algebra.Polynomial.Basis
 public import Mathlib.RingTheory.Localization.Algebra
 
@@ -73,50 +74,6 @@ lemma isInteger_mul_iff_right {f : R[X]} (hf : IsPrimitive f) (g : K[X]) :
     IsInteger R[X] (f.map (algebraMap R K) * g) ↔ IsInteger R[X] g := by
   convert isInteger_mul_iff_left hf g using 2
   rw [mul_comm]
-
-end
-
-section
-
-variable {A R : Type*} [CommRing R] [CommRing A] [Algebra R A]
-variable {ι : Type*} {s : Finset ι} {f : ι → R[X]} {v : ι → A}
-
-lemma degree_sum_eq_of_linearIndepOn (h : LinearIndepOn R v s) :
-    (∑ i ∈ s, v i • (f i).map (algebraMap R A)).degree = s.sup (fun i ↦ (f i).degree) := by
-  apply le_antisymm
-  · exact (degree_sum_le s _).trans <| Finset.sup_le fun i hi ↦ (degree_smul_le _ _).trans <|
-      degree_map_le.trans <| Finset.le_sup (f := fun i ↦ (f i).degree) hi
-  · apply Finset.sup_le
-    intro i hi
-    by_cases hf : f i = 0
-    · rw [hf, degree_zero]
-      exact OrderBot.bot_le _
-    rw [degree_eq_natDegree hf]
-    apply le_degree_of_ne_zero
-    rw [finset_sum_coeff]
-    conv =>
-      lhs; rhs; enter [x];
-      rw [coeff_smul, smul_eq_mul, coeff_map, mul_comm, ← Algebra.smul_def]
-    intro H
-    exact hf (leadingCoeff_eq_zero.mp (linearIndepOn_finset_iff.mp h _ H i hi))
-
-lemma natDegree_sum_eq_of_linearIndepOn (h : LinearIndepOn R v s) :
-    (∑ i ∈ s, v i • (f i).map (algebraMap R A)).natDegree = s.sup (fun i ↦ (f i).natDegree) := by
-  apply le_antisymm
-  · refine natDegree_sum_le_of_forall_le _ _ fun i hi ↦ (natDegree_smul_le _ _).trans <|
-      natDegree_map_le.trans <| Finset.le_sup (f := fun i ↦ (f i).natDegree) hi
-  · apply Finset.sup_le
-    intro i hi
-    by_cases hf : f i = 0
-    · rw [hf, natDegree_zero]
-      exact Nat.zero_le _
-    apply le_natDegree_of_ne_zero
-    rw [finset_sum_coeff]
-    conv =>
-      lhs; rhs; enter [x];
-      rw [coeff_smul, smul_eq_mul, coeff_map, mul_comm, ← Algebra.smul_def]
-    intro H
-    exact hf (leadingCoeff_eq_zero.mp (linearIndepOn_finset_iff.mp h _ H i hi))
 
 end
 
